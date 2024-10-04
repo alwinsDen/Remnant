b3c8062b2412f18df882d590a12a71419ef37e49
alwinsDen <jsmadesimple@gmail.com>
2024-10-05 00:54:39 +0530

    feat(ALIGNMENT): added alignment at various places to equate component for Kotlin experience.
    Signed-off-by: alwinsDen <jsmadesimple@gmail.com>

--- composeApp/src/commonMain/kotlin/org/alwinsden/remnant/App.kt
+++ composeApp/src/commonMain/kotlin/org/alwinsden/remnant/App.kt
@@ -1,5 +1,6 @@
 package org.alwinsden.remnant
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -10,16 +11,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import remnant.composeapp.generated.resources.Res
+import remnant.composeapp.generated.resources.kotlin_conf_25
 
 @Composable
 @Preview
 fun App() {
     Column(
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxWidth()
@@ -28,21 +33,72 @@ fun App() {
                 color = Color(0xFF000000)
             )
     ) {
-        Text(fontFamily = MatescFontFamily, fontSize = 64.sp, color = Color(0xFFFFFFFF), text = "Remnant")
-        OutlinedButton(
-            onClick = {},
-            colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFFFFFFF)),
-            shape = RoundedCornerShape(50),
+        Box(
+            modifier = Modifier
+                .height(85.dp)
+                .width(10.dp)
         ) {
-            Text(
-                text = "Start your experience",
-                fontFamily = InterFontFamily,
-                fontWeight = FontWeight.Normal,
-                color = Color(0xFF505050),
-                fontSize = 14.sp,
-                modifier = Modifier
-                    .padding(vertical = 5.dp, horizontal = 25.dp)
-            )
+
+        }
+        Box(
+            contentAlignment = Alignment.Center,
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(fontFamily = MatescFontFamily, fontSize = 64.sp, color = Color(0xFFFFFFFF), text = "Remnant")
+                OutlinedButton(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFFFFFFF)),
+                    shape = RoundedCornerShape(50),
+                ) {
+                    Text(
+                        text = "Start your experience",
+                        fontFamily = InterFontFamily,
+                        fontWeight = FontWeight.Normal,
+                        color = Color(0xFF505050),
+                        fontSize = 14.sp,
+                        modifier = Modifier
+                            .padding(vertical = 5.dp, horizontal = 25.dp)
+                    )
+                }
+            }
+        }
+        Box(
+            modifier = Modifier.padding(bottom = 25.dp)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = "created for",
+                    fontFamily = InterFontFamily,
+                    fontSize = 14.sp,
+                    color = Color(0xFFFFFFFF),
+                    modifier = Modifier
+                        .padding(
+                            bottom = 5.dp
+                        )
+                )
+                Image(
+                    painter = painterResource(Res.drawable.kotlin_conf_25),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .width(220.dp)
+                        .height(22.dp)
+                        .background(
+                            color = Color(0xFFFFFFFF)
+                        ),
+                    contentScale = ContentScale.FillHeight
+                )
+                Text(
+                    text = "Multiplatform Student Contest",
+                    fontFamily = InterFontFamily,
+                    fontSize = 14.sp,
+                    color = Color(0xFFFFFFFF)
+                )
+            }
         }
     }
 }
