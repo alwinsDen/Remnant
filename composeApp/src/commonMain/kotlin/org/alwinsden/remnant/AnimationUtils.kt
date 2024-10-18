b579a80f747177874dd0e7078075fa81ca8dace8
alwinsDen <jsmadesimple@gmail.com>
2024-10-18 07:31:32 +0530

    Added basic sliding animations.
    Signed-off-by: alwinsDen <jsmadesimple@gmail.com>

---
+++ composeApp/src/commonMain/kotlin/org/alwinsden/remnant/AnimationUtils.kt
@@ -0,0 +1,31 @@
+package org.alwinsden.remnant
+
+import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
+
+enum class ScaleTransitionDirection {
+    INWARDS, OUTWARDS
+}
+
+fun scaleIntoContainer(
+    direction: ScaleTransitionDirection = ScaleTransitionDirection.INWARDS,
+    initialScale: Float = if (direction == ScaleTransitionDirection.OUTWARDS) 0.9f else 1.1f
+): EnterTransition {
+    return scaleIn(
+        animationSpec = tween(220, delayMillis = 90),
+        initialScale = initialScale
+    ) + fadeIn(animationSpec = tween(220, delayMillis = 90))
+}
+
+
+fun scaleOutOfContainer(
+    direction: ScaleTransitionDirection = ScaleTransitionDirection.OUTWARDS,
+    targetScale: Float = if (direction == ScaleTransitionDirection.INWARDS) 0.9f else 1.1f
+): ExitTransition {
+    return scaleOut(
+        animationSpec = tween(
+            durationMillis = 220,
+            delayMillis = 90
+        ), targetScale = targetScale
+    ) + fadeOut(tween(delayMillis = 90))
+}
--- composeApp/src/commonMain/kotlin/org/alwinsden/remnant/RoutingControl.kt
+++ composeApp/src/commonMain/kotlin/org/alwinsden/remnant/RoutingControl.kt
@@ -1,5 +1,7 @@
 package org.alwinsden.remnant
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -13,8 +15,48 @@ fun RoutingControl() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = NavRouteClass.EntryScreen2.route) {
         //HOME: the main entry point of the application.
-        composable(NavRouteClass.Home.route) { Home(navController = navController) }
-        composable(NavRouteClass.EntryScreen1.route) { EntryScreen1(navController = navController) }
-        composable(NavRouteClass.EntryScreen2.route) { EntryScreen2(navController = navController) }
+        composable(
+            route = NavRouteClass.Home.route,
+            enterTransition = {
+                scaleIntoContainer()
+            },
+            exitTransition = {
+                scaleOutOfContainer(direction = ScaleTransitionDirection.INWARDS)
+            },
+            popEnterTransition = {
+                scaleIntoContainer(direction = ScaleTransitionDirection.OUTWARDS)
+            },
+            popExitTransition = {
+                scaleOutOfContainer()
+            }
+        ) { Home(navController = navController) }
+        composable(
+            route = NavRouteClass.EntryScreen1.route,
+            enterTransition = {
+                slideIntoContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Left,
+                    animationSpec = tween(700)
+                )
+            },
+            exitTransition = {
+                slideOutOfContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Left,
+                    animationSpec = tween(700)
+                )
+            },
+            popEnterTransition = {
+                slideIntoContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Right,
+                    animationSpec = tween(700)
+                )
+            },
+            popExitTransition = {
+                slideOutOfContainer(
+                    AnimatedContentTransitionScope.SlideDirection.Right,
+                    animationSpec = tween(700)
+                )
+            }
+        ) { EntryScreen1(navController = navController) }
+        composable(route = NavRouteClass.EntryScreen2.route) { EntryScreen2(navController = navController) }
     }
 }
