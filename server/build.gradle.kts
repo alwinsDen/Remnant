72ecb6a5a179b55da4cb1b796078af70b87ed029
alwinsDen <jsmadesimple@gmail.com>
2024-10-29 09:12:53 +0530

    bundled exposed libs.
    Signed-off-by: alwinsDen <jsmadesimple@gmail.com>

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -12,7 +12,6 @@ application {
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=${extra["io.ktor.development"] ?: "false"}")
 }
 
-val exposedVersion: String by project
 dependencies {
     implementation(projects.shared)
     implementation(libs.logback)
@@ -22,13 +21,6 @@ dependencies {
     testImplementation(libs.kotlin.test.junit)
     implementation(libs.ktor.server.content.negotiation)
     implementation(libs.ktor.serialization.kotlinx.json)
-    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-crypt:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-json:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-money:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-spring-boot-starter:$exposedVersion")
+    implementation(libs.bundles.exposed)
     implementation("org.postgresql:postgresql:42.7.4")
 }
