8d7e6b48f34a3edd20fd99de31eab3fb13ff36f3
alwinvarghese <jsmadesimple@gmail.com>
2024-11-01 16:29:32 +0530

    reformated functions, removed courtine for blocked runner.

--- server/src/main/kotlin/org/alwinsden/remnant/Application.kt
+++ server/src/main/kotlin/org/alwinsden/remnant/Application.kt
@@ -1,8 +1,14 @@
 package org.alwinsden.remnant
 
+import com.auth0.jwk.JwkProviderBuilder
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
 import io.ktor.server.engine.*
+import io.ktor.server.http.content.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.request.*
@@ -15,6 +21,8 @@ import org.alwinsden.remnant.api_data_class.TestRequest
 import org.alwinsden.remnant.controlUtils.configurationFirebase
 import org.alwinsden.remnant.models.User.UserSchemaService
 import org.alwinsden.remnant.models.configureDatabase
+import java.io.File
+import java.util.concurrent.TimeUnit
 
 fun main() {
     embeddedServer(Netty, port = SERVER_PORT, host = "localhost", module = Application::module)
@@ -22,9 +30,9 @@ fun main() {
 }
 
 fun Application.module() {
+    val applicationConfiguration = environment.config
     configurationFirebase()
     val database = configureDatabase()
-
     //enable schemas
     UserSchemaService(database)
 
@@ -46,11 +54,11 @@ fun Application.module() {
         post("/auth") {
             val req = call.receive<AuthPost>()
             println("${req.authMachine} ${req.authCode}")
-            if(req.authMachine=="DESKTOP"){
-                desktopAuth(accessToken = req.authCode)
-            } else if (req.authMachine=="ANDROID") {
-                androidAuth(accessToken = req.authCode)
-            }
+            generalAuthenticator(
+                accessToken = req.authCode,
+                authMachine = req.authMachine,
+                applicationConfig = applicationConfiguration
+            )
             call.respond(
                 MessageResponseClass(
                     200,
--- server/src/main/kotlin/org/alwinsden/remnant/Authenticator.kt
+++ server/src/main/kotlin/org/alwinsden/remnant/Authenticator.kt
@@ -1,9 +1,14 @@
 package org.alwinsden.remnant
 
+import com.auth0.jwk.JwkProviderBuilder
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.server.application.*
+import io.ktor.server.config.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
 import okhttp3.OkHttp
@@ -13,8 +18,23 @@ import org.alwinsden.remnant.api_data_class.GCloudAuthResult
 import org.alwinsden.remnant.models.User.ExposedUser
 import org.alwinsden.remnant.models.User.UserSchemaService
 import org.alwinsden.remnant.models.configureDatabase
+import java.security.KeyFactory
+import java.security.interfaces.RSAPrivateKey
+import java.security.interfaces.RSAPublicKey
+import java.security.spec.PKCS8EncodedKeySpec
+import java.util.Base64
+import java.util.Date
+import java.util.concurrent.TimeUnit
 
-fun desktopAuth(accessToken: String) {
+fun generalAuthenticator(accessToken: String, authMachine: String, applicationConfig: ApplicationConfig) {
+    if (authMachine == "DESKTOP") {
+        var res = desktopAuth(accessToken = accessToken)
+    } else if (authMachine == "ANDROID") {
+        androidAuth(accessToken = accessToken)
+    }
+}
+
+fun desktopAuth(accessToken: String): Boolean {
     val client = OkHttpClient()
     val database = configureDatabase()
     val userInstance = UserSchemaService(database)
@@ -29,7 +49,7 @@ fun desktopAuth(accessToken: String) {
             val userDetails = responseBody?.let {
                 Json.decodeFromString<GCloudAuthResult>(it)
             }
-            CoroutineScope(Dispatchers.IO).launch {
+            return runBlocking {
                 val userExists = userDetails?.let { userInstance.readEmail(it.email) }
 
                 //adds the user if the user doesn't exist in the Remnant user DB.
@@ -39,14 +59,19 @@ fun desktopAuth(accessToken: String) {
                             email = userDetails.email, name = userDetails.name
                         )
                     )
-                } else if (userExists!=null){
+                    true
+                } else if (userExists != null) {
                     println("The user exists in the database.")
+                    true
+                } else {
+                    false
                 }
             }
         } else {
             println("Failed to verify the authenticity of the user.")
         }
     }
+    return false
 }
 
 fun androidAuth(accessToken: String) {
